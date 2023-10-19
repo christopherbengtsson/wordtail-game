66f3ed2585e3f5822df7bd65ea8deaa76e4175b1
Christopher Bengtsson <christopher.bengtsson@trustly.com>
2023-10-19 20:59:21 +0200

    rename win95 icons

---
+++ src/components/assets/img/index.ts
@@ -0,0 +1,5 @@
+import JoystickIcon from '../img/win95/Joys/Joy_pressed.ico';
+import UserIcon from '../img/win95/People/User.ico';
+import AgentIcon from '../img/win95/People/Agent.ico';
+
+export { JoystickIcon, UserIcon, AgentIcon };
