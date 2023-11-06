be553fac7161415ac095361870f949424d36b688
Christopher Bengtsson <christopher.bengtsson@trustly.com>
2023-11-06 22:27:43 +0100

    playwright ci

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,4 +1,12 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: resolve(__dirname, '..', '.env.e2e') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
