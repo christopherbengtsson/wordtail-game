57f5922e2779a246907fd5e5f39a96bcf3a2714e
Christopher Bengtsson <christopher.bengtsson@trustly.com>
2023-11-12 21:21:22 +0100

    increase default base time

--- src/utils/getTimeToPlayDuration.ts
+++ src/utils/getTimeToPlayDuration.ts
@@ -1,3 +1,4 @@
+const BASE_TIME = 15 * 1000; // 15 sec
 // 1. Logarithmic Increase
 // One idea is to increase the time logarithmically based on the number of letters.
 // This means that initially, for the first few letters, the player gets a significant boost in time, but
@@ -9,10 +10,9 @@
 // 3 letters: 6747ms
 // ... and so on
 export function logarithmicDuration(numLetters: number) {
-  const baseTime = 10000; // 3 seconds for 0 letters
   const logMultiplier = 2000; // Multiplier to control the rate of increase
 
-  return baseTime + logMultiplier * Math.log(numLetters + 1);
+  return BASE_TIME + logMultiplier * Math.log(numLetters + 1);
 }
 
 // 2. Quadratic Increase
@@ -25,10 +25,9 @@ export function logarithmicDuration(numLetters: number) {
 // 3 letters: 7500ms
 // ... and so on
 export function quadraticDuration(numLetters: number) {
-  const baseTime = 3000; // 3 seconds for 0 letters
   const quadraticMultiplier = 500; // Multiplier to control the rate of increase
 
-  return baseTime + quadraticMultiplier * numLetters * numLetters;
+  return BASE_TIME + quadraticMultiplier * numLetters * numLetters;
 }
 
 // 3. Sigmoid Function
@@ -44,11 +43,11 @@ export function sigmoidDuration(numLetters: number) {
   function sigmoid(x: number) {
     return 1 / (1 + Math.exp(-x));
   }
-  const baseTime = 3000;
+
   const maxExtraTime = 10000; // The most extra time that can be given
   const scale = 0.5; // Controls the "stretch" of the sigmoid curve
 
-  return baseTime + maxExtraTime * (sigmoid(scale * numLetters) - sigmoid(0));
+  return BASE_TIME + maxExtraTime * (sigmoid(scale * numLetters) - sigmoid(0));
 }
 
 // 4. Piecewise Function
@@ -62,10 +61,9 @@ export function sigmoidDuration(numLetters: number) {
 // 4 letters: 7500ms
 // ... and so on
 export function piecewiseDuration(numLetters: number) {
-  const baseTime = 3000;
-  if (numLetters <= 3) return baseTime + numLetters * 1000;
-  if (numLetters <= 7) return baseTime + 3 * 1000 + (numLetters - 3) * 1500;
-  return baseTime + 3 * 1000 + 4 * 1500 + (numLetters - 7) * 2000;
+  if (numLetters <= 3) return BASE_TIME + numLetters * 1000;
+  if (numLetters <= 7) return BASE_TIME + 3 * 1000 + (numLetters - 3) * 1500;
+  return BASE_TIME + 3 * 1000 + 4 * 1500 + (numLetters - 7) * 2000;
 }
 
 // 5. Random Element
@@ -77,9 +75,8 @@ export function piecewiseDuration(numLetters: number) {
 // 2 letters: 6000ms
 // ... and so on
 export function randomDuration(numLetters: number) {
-  const baseTime = 3000;
   const timePerLetter = 1000;
   const randomTime = Math.random() * 2000; // Between 0 and 2 seconds
 
-  return baseTime + numLetters * timePerLetter + randomTime;
+  return BASE_TIME + numLetters * timePerLetter + randomTime;
 }
